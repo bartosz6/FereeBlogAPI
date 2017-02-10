d1a42ca00419d758ce5dcc42fc2a8ad6bbf2019d
Bartosz Kowalczyk <kontakt@feree.pl>
2017-02-10 23:17:09 +0100

    add child comment

--- Domain/Comment/Comment.cs
+++ Domain/Comment/Comment.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Domain.User;
 
 namespace Domain.Comment
@@ -7,5 +9,27 @@ namespace Domain.Comment
         public Domain.Post.Post Post {get; set;}
         public ApplicationUser Author { get; set; }
         public string Content { get; set; }
+        public Comment Parent {get; set;}
+        public IEnumerable<Comment> Childrens {get; set;}
+
+        public Comment()
+        {
+            Childrens = new List<Comment>();
+        }
+
+        public Comment(Domain.Post.Post post, ApplicationUser author, string content, Comment parent = null): this()
+        {
+            Post = post;
+            Author = author;
+            Content = content;
+            Parent = parent;
+        }
+
+        void AddChildrenComment(Comment children) {
+            children.Parent = this;
+            var newChildrens = Childrens.ToList();
+            newChildrens.Add(children);
+            Childrens = newChildrens;
+        }
     }
 }
