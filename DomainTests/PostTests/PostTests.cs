acdd968c4be30f734a1d733275d224f9ea039934
Bartosz Kowalczyk <kontakt@feree.pl>
2017-02-13 19:41:29 +0100

    tst

--- DomainTests/PostTests/PostTests.cs
+++ DomainTests/PostTests/PostTests.cs
@@ -6,12 +6,13 @@ namespace DomainTests.PostTests
 {
     public class PostTests
     {
-        private Post Post => new Post();
 
         [Fact]
         public void GetTags__gets_tags_from_text() {
-            var content = "lorem#tag1 ipsum dolor sit amet. #tag2";
-            var tags = Post.GetTags(content).ToList();
+            var post = new Post();
+            post.Content = "lorem#tag1 ipsum dolor sit amet. #tag2";
+
+            var tags = post.GetTags().ToList();
 
             Assert.Equal("tag1", tags[0].Name);
             Assert.Equal("tag2", tags[1].Name);
@@ -19,8 +20,10 @@ namespace DomainTests.PostTests
 
         [Fact]
         public void GetTags__tags_are_distinctive() {
-            var content = "lorem #tag1 #tag1 ipsum #tag1 dolor sit #tag2 amet. #tag2";
-            var tags = Post.GetTags(content).ToList();
+            var post = new Post();
+            post.Content = "lorem #tag1 #tag1 ipsum #tag1 dolor sit #tag2 amet. #tag2";
+            
+            var tags = post.GetTags().ToList();
 
             Assert.Equal("tag1", tags[0].Name);
             Assert.Equal("tag2", tags[1].Name);
